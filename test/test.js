c45a6e714b222a3001bf1c1d87596474f57b51cb
allen <yaokailun@ifanr.com>
2017-06-19 00:20:08 +0800

    init test case

---
+++ test/test.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var removeConsole = require('../index');
+
+let source = ''
+
+describe('remove console', function() {
+  describe('no option', function() {
+    it('should return a empty string when the code just a console statement', function() {
+      source = `console.log('something')`;
+      assert.equal('', removeConsole(source));
+    });
+    it('should return source when the code just a comment including console', function() {
+      source = `// console.log('something')`;
+      assert.equal(source, removeConsole(source));
+    });
+    it('should return source when the code just a string including console', function() {
+      source = `"console.log('something')"`;
+      assert.equal(source, removeConsole(source));
+    });
+  });
+});
